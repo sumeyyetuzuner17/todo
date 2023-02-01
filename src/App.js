a3ffab674ce68448cd314366cd4c11b9af5742f8
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-02-01 15:25:05 +0300

    ToDoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+    if (!newItem) {
+      alert("Lütfen yapılacak bir şey ekleyin");
+      return;
+    }
+    const item = {
+      id: Math.floor(Math.random() * 100),
+      value: newItem,
+    };
+    setItems((e) => [...e, item]);
+    setNewItem("");
+  }
+
+  const onChange = (e) => {
+    setNewItem(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="add an item.."
+          value={newItem}
+          onChange={onChange}
+        ></input>
+      </div>
+      <div>
+        <button onClick={() => addItem()}>EKLE</button>
+      </div>
+
+      <ul>
+        {items.map((item) => {
+          return <li key={item.id}>{item.value}</li>;
+        })}
+      </ul>
     </div>
   );
 }
